1e4640f8a69fc116619ddd258de3e3fcc1f1f197
Victini378 <victini378@gmail.com>
2019-04-13 23:11:13 +0200

    Added Python Implement

---
+++ Python/SPR.py
@@ -0,0 +1,28 @@
+v = []
+
+def sumExists(sum):
+    if sum == 0:
+        return True
+
+    for i in v:
+        if i <= sum and sumExists(sum - i):
+            return True
+
+    return False
+
+def iteration():
+    for i in range(99, 1, -1):
+        print(i)
+        if not sumExists(i):
+            return i
+    return 0
+
+f = open("input.txt", 'r')
+
+next(f)
+
+for i in f:
+    v.append(int(i))
+
+f = open("output.txt", 'w')
+f.write(str(iteration()))
